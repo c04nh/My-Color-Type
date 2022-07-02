e0dd67842ffa85d11517f082dde7fb12b033fa87
c04nh <w2038@e-mirim.hs.kr>
2022-07-03 01:39:28 +0900

    setting 변경

--- MyColorType/settings.py
+++ MyColorType/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%l11^alkrvzk909ut_!wfivelz%7u4o!#yv-4b-tt^s)75h^3m'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn MyColorType.wsgi --log-file -
